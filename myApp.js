f80042255ed0ebb9b0e6b5c8c6c58bb84e98008e
jade@itnt.co.za <jade@itnt.co.za>
2022-04-11 19:21:21 +0200

    chaining middleware commands

--- myApp.js
+++ myApp.js
@@ -24,6 +24,13 @@ app.get(`/json`, function (req, res) {
     } else {
         res.json({"message": "Hello json"});
     }
+});
+
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.json({"time": req.time});
 })
 
 
