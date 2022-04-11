9b4b81e089acdd413250b5cfd8696c76a00cf7d4
jade@itnt.co.za <jade@itnt.co.za>
2022-04-11 10:03:23 +0200

    creating a /json route and serving a json browser

--- myApp.js
+++ myApp.js
@@ -10,7 +10,11 @@ app.get('/', function(req,res) {
     res.sendFile(`${__dirname}/views/index.html`);
 });
 
-app.use('/public', express.static(`${__dirname}/public`))
+app.use('/public', express.static(`${__dirname}/public`));
+
+app.get(`/json`, function (req, res) {
+    res.json({"message": "Hello json"});
+})
 
 
 
